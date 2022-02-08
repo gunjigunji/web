6c8fd7d20f7270c72ede0c39b77b5a51ccfc4f88
y_gunji <y_gunji@hep.s.kanazawa-u.ac.jp>
2022-02-09 02:29:47 +0900

    added gtag.js

---
+++ js/gtag.js
@@ -0,0 +1,8 @@
+//Global site tag (gtag.js) - Google Analytics
+document.write(<script async src="https://www.googletagmanager.com/gtag/js?id=UA-219712631-1"></script>);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-219712631-1');
